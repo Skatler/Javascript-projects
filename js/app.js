fc8c276f67499b47f9f79190c3f2a68b97c2e3b7
Skatler <yaroslavzozuliya95@gmail.com>
2023-04-17 15:34:24 +0400

    Commentary

--- js/app.js
+++ js/app.js
@@ -1,20 +1,21 @@
-const express = require('express');
-const sharp = require('sharp');
-const path = require('path');
-const multer = require('multer');
+const express = require('express'); // импортируем Express, фреймворк для создания сервера
+const sharp = require('sharp'); // импортируем библиотеку Sharp для обработки изображений
+const path = require('path'); // импортируем модуль path для работы с путями файловой системы
+const multer = require('multer'); // импортируем библиотеку multer для обработки файлов, отправленных в форме
+const app = express(); // создаем новый сервер
+const upload = multer(); // создаем экземпляр multer
 
-const app = express();
-const upload = multer();
+app.use(express.static(path.join(__dirname, 'public'))); // указываем Express использовать папку public как статическую
 
-app.use(express.static(path.join(__dirname, 'public')));
-
+// создаем маршрут для обработки POST-запросов на /convert, используя multer для обработки изображений, отправленных в форме
 app.post('/convert', upload.single('image'), (req, res) => {
-  if (!req.file) {
+  if (!req.file) { // если файл не был загружен, отправляем клиенту ошибку 400
     return res.status(400).send('No image uploaded');
   }
 
-  const image = req.file;
+  const image = req.file; // получаем объект файла из запроса
 
+  // используем Sharp для преобразования изображения в формат WebP и отправки его обратно клиенту
   sharp(image.buffer)
     .webp()
     .toBuffer()
@@ -31,11 +32,13 @@ app.post('/convert', upload.single('image'), (req, res) => {
     });
 });
 
+// создаем маршрут для обработки GET-запросов на корневой URL и отправки HTML-страницы клиенту
 app.get('/', (_req, res) => {
   const indexPath = path.resolve(__dirname, 'public', 'index.html');
   res.sendFile(indexPath);
 });
 
+// запускаем сервер на порту 3000
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
