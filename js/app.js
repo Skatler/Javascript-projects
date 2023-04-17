211b670042c40c3630c1a54e15469acbad1777b4
Skatler <yaroslavzozuliya95@gmail.com>
2023-04-17 16:29:18 +0400

    v 0.2

--- js/app.js
+++ js/app.js
@@ -1,21 +1,20 @@
-const express = require('express'); // импортируем Express, фреймворк для создания сервера
-const sharp = require('sharp'); // импортируем библиотеку Sharp для обработки изображений
-const path = require('path'); // импортируем модуль path для работы с путями файловой системы
-const multer = require('multer'); // импортируем библиотеку multer для обработки файлов, отправленных в форме
-const app = express(); // создаем новый сервер
-const upload = multer(); // создаем экземпляр multer
+const express = require('express');
+const sharp = require('sharp');
+const path = require('path');
+const multer = require('multer');
 
-app.use(express.static(path.join(__dirname, 'public'))); // указываем Express использовать папку public как статическую
+const app = express();
+const upload = multer();
 
-// создаем маршрут для обработки POST-запросов на /convert, используя multer для обработки изображений, отправленных в форме
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.post('/convert', upload.single('image'), (req, res) => {
-  if (!req.file) { // если файл не был загружен, отправляем клиенту ошибку 400
+  if (!req.file) {
     return res.status(400).send('No image uploaded');
   }
 
-  const image = req.file; // получаем объект файла из запроса
+  const image = req.file;
 
-  // используем Sharp для преобразования изображения в формат WebP и отправки его обратно клиенту
   sharp(image.buffer)
     .webp()
     .toBuffer()
@@ -32,13 +31,13 @@ app.post('/convert', upload.single('image'), (req, res) => {
     });
 });
 
-// создаем маршрут для обработки GET-запросов на корневой URL и отправки HTML-страницы клиенту
 app.get('/', (_req, res) => {
   const indexPath = path.resolve(__dirname, 'public', 'index.html');
   res.sendFile(indexPath);
 });
 
-// запускаем сервер на порту 3000
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
+
+
